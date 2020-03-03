755409acf4f286ddae66f23a6ae0eb4f8cf1fa60
Nicolas Harraudeau <nicolas.harraudeau@sonarsource.com>
2020-03-03 17:35:25 +0100

    Retrieve all rules with secondary locations

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='secondary_locations_without_messages',
+      version='0.1',
+      description='secondary_locations_without_messages',
+      url='http://example.com',
+      author='proj',
+      author_email='proj@example.com',
+      license='MIT',
+      packages=['sec_loc'],
+      install_requires=['requests'],
+      zip_safe=False)
